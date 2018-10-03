f50df4af968c4f082d020de361d1838093845309
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-03 12:44:12 +0500

    writing to console

---
+++ 01-WritingToConsole/01-WritingToConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_WritingToConsole
+{
+    class Program
+    {
+        static void Main1()
+        {
+            Console.WriteLine("Hello C#  from Main1");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#  from Main");
+            Main1();
+        }
+    }
+}
