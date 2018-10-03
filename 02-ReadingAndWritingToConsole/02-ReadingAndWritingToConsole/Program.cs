0d359d903e955eefee707d9be242d40c4442e65f
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-03 12:45:59 +0500

    reading and writing to console

---
+++ 02-ReadingAndWritingToConsole/02-ReadingAndWritingToConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ReadingAndWritingToConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            // Concatenation
+            Console.WriteLine("Hello, " + firstName + " " + lastName + "!");
+            // Placeholder syntax
+            Console.WriteLine("Hello, {0} {1}!", firstName, lastName);
+        }
+    }
+}
