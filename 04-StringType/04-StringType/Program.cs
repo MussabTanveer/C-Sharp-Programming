396e864aaf91a55e601959905e518437517ffc09
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-03 12:48:00 +0500

    string data type

---
+++ 04-StringType/04-StringType/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_StringType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            Console.WriteLine(name);
+
+            // Escape Sequences
+            name = "\"John\"";
+            Console.WriteLine(name);
+
+            name = "One\nTwo\nThree";
+            Console.WriteLine(name);
+
+            name = "C:\\Users\\Spectacular\\Documents\\Visual Studio 2015\\Projects\\C# Tutorials";
+            Console.WriteLine(name);
+
+            // Verbatim Literal
+            name = @"C:\\Users\\Spectacular\\Documents\\Visual Studio 2015\\Projects\\C# Tutorials";
+            Console.WriteLine(name);
+
+            name = @"C:\Users\Spectacular\Documents\Visual Studio 2015\Projects\C# Tutorials";
+            Console.WriteLine(name);
+        }
+    }
+}
