6d3a15e64d8a8296eb627939442b3e2767ea65a3
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-03 19:36:34 +0500

    arrays

---
+++ 08-Arrays/08-Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] evenNumbers = new int[3];
+            evenNumbers[0] = 0;
+            evenNumbers[1] = 2;
+            evenNumbers[2] = 4;
+
+            //Console.WriteLine(evenNumbers[1]);
+
+            int[] marks = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(marks[2]);
+
+            int[] qty = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(qty[2]);
+
+            int[] students = new[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(students[2]);
+        }
+    }
+}
