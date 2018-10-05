8d1c6daaa6c3966e981a0796507f9defa2655529
MussabTanveer <mussab.tanveer9@gmail.com>
2018-10-05 20:38:47 +0500

    while loop

---
+++ 11-WhileLoop/11-WhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your target: ");
+            int target = int.Parse(Console.ReadLine());
+
+            int start = 0;
+            while(start <= target)
+            {
+                Console.WriteLine(start);
+                start += 2;
+            }
+        }
+    }
+}
